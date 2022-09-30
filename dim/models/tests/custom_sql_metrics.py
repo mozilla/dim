dd56f20e7d4f17e4a64b936c7a4de284a91746b4
Alekhya Kommasani <akommasani@mozilla.com>
2022-09-30 14:35:03 -0400

    Add custom sql metrics

---
+++ dim/models/tests/custom_sql_metrics.py
@@ -0,0 +1,17 @@
+from models.tests.base import Base
+
+## TODO: validate config, correct keys + types
+class CustomSqlMetrics(Base):
+    TEMPLATE_FILE = "custom_sql_metrics" + Base.TEMPLATE_FILE_EXTENSION
+
+    def __init__(self, project_id, dataset_id, table_id, config):
+        super().__init__(config)
+        self.config["partition"] = "2020-01-13"
+        self.config["project_id"] = project_id
+        self.config["dataset_id"] = dataset_id
+        self.config["table_id"] = table_id
+        self.config["dq_check"] = "custom_sql_metric"
+        self.name = dataset_id + '__' + "custom_sql_metric"
+
+    def execute_test_sql(self, sql):
+        return super().execute_test_sql(sql=sql)
