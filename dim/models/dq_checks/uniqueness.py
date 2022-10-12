18a3ed73fb625c942b7fbe13a3e5654cb0e9d733
Alekhya Kommasani <akommasani@mozilla.com>
2022-10-11 21:34:58 -0400

    refactor the code to rename tests to dq_checks

--- dim/models/dq_checks/uniqueness.py
+++ dim/models/dq_checks/uniqueness.py
@@ -1,8 +1,8 @@
-from dim.models.tests.base import Base
+from dim.models.dq_checks.base import Base
 
 ## TODO: validate config, correct keys + types
-class TableRowCount(Base):
-    TEMPLATE_FILE = "table_row_count" + Base.TEMPLATE_FILE_EXTENSION
+class Uniqueness(Base):
+    TEMPLATE_FILE = "uniqueness" + Base.TEMPLATE_FILE_EXTENSION
 
     def __init__(self, project_id, dataset_id, table_id, dataset_owner, config):
         super().__init__(config)
@@ -10,12 +10,12 @@ class TableRowCount(Base):
         self.config["project_id"] = project_id
         self.config["dataset_id"] = dataset_id
         self.config["table_id"] = table_id
-        self.config["dq_check"] = "table_row_count"
-        self.config["dataset_owner"] = "dataset_owner"
-        self.name = dataset_id + '__' + "table_row_count"
+        self.config["dq_check"] = "uniqueness"
+        self.config["dataset_owner"] = dataset_owner
+        self.name = dataset_id + '__' + "uniqueness"
 
     def generate_test_sql(self):
-        return super().generate_test_sql(dq_check="table_row_count")
+        return super().generate_test_sql(dq_check="uniqueness")
 
     def execute_test_sql(self, sql):
         return super().execute_test_sql(sql=sql)
--- tests/test_not_null.py
+++
@@ -1,1 +0,0 @@
-def test_dummy(): pass
