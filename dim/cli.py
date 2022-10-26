0bbe319dbebbd3a1ef7fa182cf0c4bc75486989a
kik-kik <kignasiak01@gmail.com>
2022-10-26 14:19:37 +0100

    added dim_config model and using that instead

--- dim/cli.py
+++ dim/cli.py
@@ -7,6 +7,7 @@ import yaml
 
 import dim.error as error
 from dim.bigquery_client import BigQueryClient
+from dim.models.dim_config import DimConfig
 from dim.models.dq_checks.custom_sql_metrics import CustomSqlMetrics
 from dim.models.dq_checks.not_null import NotNull
 from dim.models.dq_checks.table_row_count import TableRowCount
@@ -119,35 +120,44 @@ def run(project, dataset, table, date_partition_parameter):
             f"Starting the data checks - {project}, {dataset}, {table}"
         )
         for config in config["dim_config"]:
-            # TODO: validate config, correct keys + types --add a function
-            dataset_owner = config["owner"]["email"]
-            for test in config["tests"]:
-                test_type = test["type"]
+            dim_config = DimConfig(**config)
+
+            dataset_owner = dim_config.owner
+
+            for dim_test in config["dim_tests"]:
+                test_type = dim_test["type"]
+                logging.info(dim_test["options"])
+
                 dq_check = TEST_CLASS_MAPPING[test_type](
                     project=project,
                     dataset=dataset,
                     table=table,
-                    config=test["config"],
-                    dataset_owner=dataset_owner,
+                    config=dim_test["options"],
+                    dataset_owner=dataset_owner["email"],
                     date_partition_parameter=date_partition_parameter,
                 )
+
                 _, test_sql = dq_check.generate_test_sql()
-                dq_check.execute_test_sql(sql=test_sql)
+
+                # dq_check.execute_test_sql(sql=test_sql)
                 logging.info(f"Running DQ check - {test_type}")
-                is_slack_enabled = test["config"]["slack_alert"].lower()
+
+                is_slack_enabled = dim_test["options"]["slack_alert"].lower()
                 logging.info(f"Slack notification has been {is_slack_enabled}")
+
                 if is_slack_enabled == "enabled":
-                    slack_handles = config["owner"]["slack_handle"]
-                    channel = test["config"]["channel"]
-                    send_slack_alert(
-                        channel,
-                        project,
-                        dataset,
-                        table,
-                        test_type,
-                        slack_handles,
-                        date_partition_parameter,
-                    )
+                    slack_handles = dataset_owner["slack_handle"]
+                    channel = dim_test["options"]["channel"]
+                    #         send_slack_alert(
+                    #             channel,q
+                    #             project,
+                    #             dataset,
+                    #             table,
+                    #             test_type,
+                    #             slack_handles,
+                    #             date_partition_parameter,
+                    #         )
+                    logging.info(f"Slack handle {slack_handles} ")
                     logging.info(f"Users notified via {channel} ")
 
 
