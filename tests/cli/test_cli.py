1d0f63ee6957e70ad84860dc44c03224c1c1ed17
kik-kik <kignasiak01@gmail.com>
2022-10-31 16:58:30 +0100

    added tests for run cmd

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -2,7 +2,7 @@ import pytest
 from unittest.mock import patch
 
 from click.testing import CliRunner
-from dim.cli import backfill
+from dim.cli import backfill, run
 from dim.error import DateRangeException
 import datetime
 
@@ -49,3 +49,35 @@ def test_backfill(run_patch, runner, backfill_settings, start, end, expected_cal
     if actual_exit_code == 0:
         run_patch.call_args_list[0][0] == (*backfill_settings, datetime.datetime(*map(int, start.split("-"))))
         run_patch.call_args_list[-1][0] == (*backfill_settings, datetime.datetime(*map(int, end.split("-"))))
+
+
+
+@pytest.fixture
+def run_settings():
+    return ("data-monitoring-dev", "dummy", "active_users_aggregates_v1",)
+
+
+@patch("dim.cli.run_check")
+@pytest.mark.parametrize(
+    "date,expected_exit_code,expected_exception", [
+        ("2022-01-01", 0, None),
+        ("2022/01/01", 2, SystemExit),
+        (None, 2, SystemExit),
+    ]
+)
+
+def test_run(run_patch, runner, run_settings, date, expected_exit_code, expected_exception):
+    cmd_args = "--project={} --dataset={} --table={}".format(*run_settings)
+    cmd_args += f" --date_partition_parameter={date}" if date else ""
+
+    result = runner.invoke(run, cmd_args.split(" "))
+
+    actual_exit_code = result.exit_code
+
+    if actual_exit_code != 0:
+        assert result.exc_info[0] == expected_exception
+
+    assert actual_exit_code == expected_exit_code
+
+    if actual_exit_code == 0:
+        run_patch.assert_called_once_with(*run_settings, datetime.datetime(*map(int, date.split("-"))))
