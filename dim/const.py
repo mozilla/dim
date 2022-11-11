918858a9ade4ccd7ebf030db0262f64a44d88c64
kik-kik <kignasiak01@gmail.com>
2022-11-11 16:41:17 +0100

    added combined column uniqueness check

--- dim/const.py
+++ dim/const.py
@@ -16,6 +16,9 @@ DIM_CHECK_CLASS_MAPPING = {
     ),
     "table_row_count": getattr(table_row_count, "TableRowCount"),  # noqa: F405
     "column_length": getattr(column_length, "ColumnLength"),  # noqa: F405
+    "combined_column_uniqueness": getattr(
+        combined_column_uniqueness, "CombinedColumnUniqueness"  # noqa: F405
+    ),
     "value_in_set": getattr(value_in_set, "ValueInSet"),  # noqa: F405
     "compare_row_count_to_table": getattr(
         compare_row_count_to_table, "CompareRowCountToTable"  # noqa: F405
